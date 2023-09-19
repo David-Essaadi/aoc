5e9bc11de4704eb5c09e6ceb0b1bda7e360d8a10
David Essaadi <davidessaadi@gmail.com>
2023-09-19 09:30:50 +0200

    Added unit tests

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -1,28 +1,26 @@
-use std::path::Path;
-
 fn main() {
-    let contents = lib::read_file_contents(Path::new("inputs/input-1.txt"));
-    solution_1(&contents);
-    solution_2(&contents);
+    let contents = lib::read_file_contents("inputs/input-1.txt");
+    let result_1 = solution_1(&contents);
+    println!("solution 1 - max: {}", result_1);
+    let result_2 = solution_2(&contents);
+    println!("solution 2 - total top three: {}", result_2);
 }
 
-fn solution_1(contents: &String) {
-    let max = contents
+fn solution_1(contents: &String) -> i32 {
+    contents
         .split("\n\n")    
         .map(get_total)
         .max()
-        .unwrap_or(0);
-    println!("solution 1 - max: {}", max)
+        .unwrap_or(0)
 }
 
-fn solution_2(contents: &String) {
+fn solution_2(contents: &String) -> i32 {
     let mut totals: Vec<i32> = contents        
         .split("\n\n")
         .map(get_total)
         .collect();
     totals.sort_unstable_by_key(|w| std::cmp::Reverse(*w));
-    let total = totals.drain(0..3).reduce(|acc, curr| acc + curr).unwrap();
-    println!("solution 2 - total top three: {}", total)
+    totals.drain(0..3).reduce(|acc, curr| acc + curr).unwrap()
 }
 
 fn get_total(inventory: &str) -> i32 {
@@ -31,4 +29,30 @@ fn get_total(inventory: &str) -> i32 {
         .map(|x| i32::from_str_radix(x, 10).unwrap_or(0))
         .reduce(|acc, curr| acc + curr)
         .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_eq;
+    use lib::read_file_contents;
+
+    use super::*;
+
+    #[test]
+    fn test_solution_1() {
+        let contents = read_file_contents("inputs/input-1.txt");
+        assert_eq!(solution_1(&contents), 71780);
+    }
+
+    #[test]
+    fn test_solution_2() {
+        let contents = read_file_contents("inputs/input-1.txt");
+        assert_eq!(solution_2(&contents), 212489);
+    }
+    
+    #[test]
+    fn test_total() {
+        let inv = "100\n200\n300";
+        assert_eq!(get_total(inv), 600)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{path::Path, fs::File, io::Read};
 
-pub fn read_file_contents(path: &Path) -> String {
+pub fn read_file_contents(file_path: &str) -> String {
+    let path = Path::new(file_path);
     let mut file = match File::open(path) {
         Ok(file) => file,
         Err(why) => panic!("unable to open {}: {}", path.display(), why),
