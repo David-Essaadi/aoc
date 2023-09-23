3d4fd46caa9ae40e5bf21cbb7dc1d4118ce51d45
David Essaadi <davidessaadi@gmail.com>
2023-09-23 23:57:35 +0200

    Day 2 - solution 2

--- src/bin/day-2.rs
+++ src/bin/day-2.rs
@@ -3,23 +3,24 @@ use std::cmp::Ordering;
 
 fn main() {
     solution_1(&lib::read_file_contents("inputs/input-2.txt"));
+    solution_2(&lib::read_file_contents("inputs/input-2.txt"));
 }
 
+fn solution_1(contents: &String) -> i32 {
+    let rounds = contents.trim().split("\n");
+    rounds.map(|r| points_for_round(&parse_round(&r))).reduce(|acc, curr| acc + curr).unwrap_or(0)
+}
+
+fn solution_2(contents: &String) -> i32 {
+    let rounds = contents.trim().split("\n");
+    rounds.map(|r| points_for_round(&parse_round_2(&r))).reduce(|acc, curr| acc + curr).unwrap_or(0)
+}
+
+#[derive(Clone, Copy, PartialEq)]
 enum RPS {
     Rock,
     Paper,
     Scissors,
-}
-
-impl PartialEq for RPS {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (RPS::Rock, RPS::Rock) => true,
-            (RPS::Paper, RPS::Paper) => true,
-            (RPS::Scissors, RPS::Scissors) => true,
-            _ => false,
-        }
-    }
 }
 
 impl PartialOrd for RPS {
@@ -38,6 +39,17 @@ impl PartialOrd for RPS {
     }
 }
 
+impl fmt::Display for RPS {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RPS::Rock => write!(f, "rock"),
+            RPS::Paper => write!(f, "paper"),
+            RPS::Scissors => write!(f, "scissors"),
+        }
+    }
+}
+
+#[derive(PartialEq)]
 enum Outcome {
     Loss,
     Draw,
@@ -51,6 +63,13 @@ impl fmt::Display for Outcome {
             Outcome::Draw => write!(f, "It's a draw"),
             Outcome::Win => write!(f, "We win"),
         }
+    }
+}
+
+struct Round(RPS, RPS);
+impl std::fmt::Display for Round {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "They play {} and we play {}.", self.0, self.1)
     }
 }
 
@@ -78,29 +97,6 @@ fn points_for_round(round: &Round) -> i32 {
     }
 }
 
-impl fmt::Display for RPS {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            RPS::Rock => write!(f, "rock"),
-            RPS::Paper => write!(f, "paper"),
-            RPS::Scissors => write!(f, "scissors"),
-        }
-    }
-}
-
-struct Round(RPS, RPS);
-impl std::fmt::Display for Round {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "They play {} and we play {}.", self.0, self.1)
-    }
-}
-
-
-fn solution_1(contents: &String) -> i32 {
-    let rounds = contents.trim().split("\n");
-    rounds.map(|r| points_for_round(&parse_round(&r))).reduce(|acc, curr| acc + curr).unwrap_or(0)
-}
-
 fn parse_round(line: &str) -> Round {
     match line {
         "A X" => Round(RPS::Rock, RPS::Rock),
@@ -116,6 +112,33 @@ fn parse_round(line: &str) -> Round {
     }
 }
 
+fn find_required_move(opponent: &RPS, desired_outcome: Outcome) -> Round {
+    let all_choices = [RPS::Rock, RPS::Paper, RPS::Scissors];
+
+    for &choice in all_choices.iter() {
+        let round = Round(*opponent, choice);
+        if outcome_of_round(&round) == desired_outcome {
+            return round
+        }
+    }
+
+    panic!("unable to find required move")
+}
+
+fn parse_round_2(line: &str) -> Round {
+    find_required_move(&match line.chars().nth(0).unwrap() {
+        'A' => RPS::Rock,
+        'B' => RPS::Paper,
+        'C' => RPS::Scissors,
+        _ => panic!("incorrect input")
+    }, match line.chars().nth(2).unwrap() {
+        'X' => Outcome::Loss,
+        'Y' => Outcome::Draw,
+        'Z' => Outcome::Win,
+        _ => panic!("incorrect input")
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use std::assert_eq;
@@ -125,7 +148,7 @@ mod tests {
 
     #[test]
     fn test_solution_1_example() {
-        let contents = read_file_contents("inputs/example-2.1.txt");
+        let contents = read_file_contents("inputs/example-2.txt");
         assert_eq!(solution_1(&contents), 15);
     }
 
@@ -133,5 +156,17 @@ mod tests {
     fn test_solution_1() {
         let contents = read_file_contents("inputs/input-2.txt");
         assert_eq!(solution_1(&contents), 10941);
+    }
+
+    #[test]
+    fn test_solution_2_example() {
+        let contents = read_file_contents("inputs/example-2.txt");
+        assert_eq!(solution_2(&contents), 12);
+    }
+    
+    #[test]
+    fn test_solution_2() {
+        let contents = read_file_contents("inputs/input-2.txt");
+        assert_eq!(solution_2(&contents), 13071);
     }
 }
